e2f2da788bea197def16e9bf7906b1c4a3f8413a
mustafaInholland <mtepeyurt03@hotmail.com>
2024-03-12 14:28:52 +0100

    Activity product

---
+++ SomerenDAL/ActivityDao.cs
@@ -0,0 +1,43 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SomerenDAL
+{
+    public class ActivityDao : BaseDao
+    {
+        public List<Activity> GetAllActivities()
+        {
+            string query = "SELECT * FROM Activiteit";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadActivities(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Activity> ReadActivities(DataTable datatable)
+        {
+            List<Activity> activities = new List<Activity>();
+
+            foreach (DataRow dr in datatable.Rows)
+            {
+                Activity activity = new Activity()
+                {
+
+                    Id = (int)dr["activiteit_Id"],
+                    Name = dr["naam"].ToString(),
+                    Date = dr["dag"].ToString(),
+                    StartTime = dr["begin_Tijd"].ToString(),
+                    EndTime = dr["eind_Tijd"].ToString()
+                };
+
+                activities.Add(activity);
+            }
+            return activities;
+        }
+    }
+}
