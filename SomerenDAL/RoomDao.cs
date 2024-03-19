ba5b1955bf2af2fd1d4e8eab4c3b98b1f3439d34
Natfaji_A <modemoon2003@hotmail.com>
2024-03-19 14:11:09 +0100

    Create room model/DAO/service

---
+++ SomerenDAL/RoomDao.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+	public class RoomDao : BaseDao
+	{
+		public List<Room> GetAllRooms()
+		{
+			string query = "SELECT * FROM [Room]";
+			SqlParameter[] sqlParameters = new SqlParameter[0];
+			return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+		}
+
+		private List<Room> ReadTables(DataTable dataTable)
+		{
+			List<Room> rooms = new List<Room>();
+
+			foreach (DataRow dr in dataTable.Rows)
+			{
+				Room room = new Room()
+				{
+					roomId = (int)dr["roomId"],
+					size = (int)dr["size"],
+					roomName = dr["roomType"].ToString(),
+					roomType = dr["roomName"].ToString()
+				};
+				rooms.Add(room);
+			}
+			return rooms;
+		}
+	}
+}
--- SomerenModel/Room.cs
+++ SomerenModel/Room.cs
@@ -1,10 +1,10 @@
 ﻿namespace SomerenModel
 {
-    public class Room
-    {
-        public int Id { get; set; }         // database id
-        public int Number { get; set; }     // RoomNumber, e.g. 206
-        public int Capacity { get; set; }   // number of beds, either 4, 6, 8, 12 or 16
-        public bool Type { get; set; }      // student = false, teacher = true
-    }
+	public class Room
+	{
+		public int roomId { get; set; }       // database id
+		public int size { get; set; }     // number of beds, either 4, 6, 8, 12 or 16
+		public string roomType { get; set; }  // student = false, teacher = true
+		public string roomName { get; set; }
+	}
 }
