0aa33a35e994433a3688106c7c078c877f4bbe05
mustafaInholland <mtepeyurt03@hotmail.com>
2024-03-19 15:31:06 +0100

    drink made

---
+++ SomerenService/DrinkService.cs
@@ -0,0 +1,41 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenService
+{
+    public class DrinksService : DrinkDao
+    {
+        private DrinkDao drinksdb;
+        DrinkDao drinksDao = new DrinkDao();
+
+        public DrinksService()
+        {
+            drinksdb = new DrinkDao();
+        }
+
+        public List<Drink> GetDrink()
+        {
+            return drinksdb.GetAllDrinks();
+        }
+
+        public void UpdateDrink(Drink drink)
+        {
+            drinksDao.UpdateDrink(drink);
+        }
+
+        public void AddDrink(Drink drink)
+        {
+            drinksDao.AddDrink(drink);
+        }
+
+        public void DeleteDrink(Drink drink)
+        {
+            drinksDao.DeleteDrink(drink);
+        }
+    }
+}
